3346fe88581229573bd4c9251ebf7ed1e3e3419e
TigerMehMat <vova9325053@mail.ru>
2018-05-06 19:40:34 +0300

    CreatePlagin

--- scrollto.js
+++ scrollto.js
@@ -1,14 +1,32 @@
-$("a.scrollto").click(function () {
-  var $element = $(this),
-      elementClick = $element.attr("href");
+(function($){
+  jQuery.fn.scrollto = function(options){
+    // Зададим список свойств и укажем для них значения по умолчанию.
+    // Если при вызове метода будут указаны пользовательские
+    // варианты некоторых из них, то они автоматически перепишут
+    // соответствующие значения по умолчанию
+    options = $.extend({
+      speed:300, //цвет элемента над которым нет курсора
+      top:1 //цвет элемента на который наведен курсор
+    }, options};
 
-  elementClick = elementClick.substring(elementClick.lastIndexOf('#'));
+    var make = function(){
+      $(this).click(function(){
+        var $element = $(this),
+            elementClick = $element.attr("href");
 
-  destination = jQuery(elementClick).offset().top+1;
-  $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 300, function (){
-    if($element.data('scrolltofocus')){
-      $($element.data('scrolltofocus')).focus();
-    }
-  });
-  return false;
-});
+        elementClick = elementClick.substring(elementClick.lastIndexOf('#'));
+
+        destination = jQuery(elementClick).offset().top+options.top;
+        $("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, options.speed, function (){
+          if($element.data('scrolltofocus')){
+            $($element.data('scrolltofocus')).focus();
+          }
+        });
+        return false;
+      });
+    };
+
+    return this.each(make);
+    // в итоге, метод responsiveBlock вернет текущий объект jQuery обратно
+  };
+})(jQuery);
